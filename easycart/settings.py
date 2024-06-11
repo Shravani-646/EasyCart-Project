d7cd5e6c539261478a5076b5908d735124b486dc
Sai Sravani Tellapuri <tssravani2001@gmail.com>
2024-06-11 11:58:34 +0530

    Setting up the Database

--- easycart/settings.py
+++ easycart/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'easycart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'easycart',
+        'HOST':'127.0.0.1',
+        'USER':'root',
+        'PASSWORD':'root'
     }
 }
 
