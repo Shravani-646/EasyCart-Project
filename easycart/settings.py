1c5e1942e5a1e258e88e01351348dbd54a4ec8d5
Sai Sravani Tellapuri <tssravani2001@gmail.com>
2024-06-12 10:23:01 +0530

    REST framework setup

--- easycart/settings.py
+++ easycart/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'store',
     'core',
     'tags',
@@ -129,3 +130,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "core.User"
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
